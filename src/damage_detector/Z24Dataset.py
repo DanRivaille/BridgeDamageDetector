5991ff7a40b865a3c126386447a13330e6bec835
Dan Santos <dan.santos.hd@outlook.com>
2025-03-31 01:41:00 -0300

    some files are reformated

--- src/damage_detector/Z24Dataset.py
+++ src/damage_detector/Z24Dataset.py
@@ -10,47 +10,43 @@ from src.damage_detector.DatasetType import DatasetType
 
 
 def stack_arrays(stacked_array: np.ndarray, new_array: np.ndarray) -> np.ndarray:
-    """
+  """
     Stacks an array inside an array stack, if the array stack is empty then returns the new array.
     @param stacked_array Current array stack (may be empty).
     @param new_array A new array to enter the stack.
     """
-    if stacked_array.shape[0] != 0:
-        return np.vstack((stacked_array, new_array))
-    else:
-        return new_array
+  if stacked_array.shape[0] != 0:
+    return np.vstack((stacked_array, new_array))
+  else:
+    return new_array
 
 
 def load(config: ConfigParams, type_dataset: DatasetType) -> np.ndarray:
-    """
+  """
     Loads the data of the Z24 dataset based on configuration parameters and dataset type.
     @param config Configuration parameters for the dataset loaded from a .json file (they are the first date and last date).
     @param type_dataset The type of dataset that the model will use (TRAIN, TEST, VALIDATION TRAIN o VALIDATION TEST).
     """
-    data_params = config.get_params('data_params').get(type_dataset.value)
-    first_date = datetime.strptime(data_params.get('first_date'), "%d/%m/%Y")
-    last_date = datetime.strptime(data_params.get('last_date'), "%d/%m/%Y")
-    sensor_number = config.get_params('data_params').get('sensor_number')
+  data_params = config.get_params('data_params').get(type_dataset.value)
+  first_date = datetime.strptime(data_params.get('first_date'), "%d/%m/%Y")
+  last_date = datetime.strptime(data_params.get('last_date'), "%d/%m/%Y")
+  sensor_number = config.get_params('data_params').get('sensor_number')
 
-    total_hours = (last_date - first_date + timedelta(days=1)) // timedelta(hours=1)
-    year = str(first_date.year)
-    data = np.empty(0)
+  total_hours = (last_date - first_date + timedelta(days=1)) // timedelta(hours=1)
+  year = str(first_date.year)
+  data = np.empty(0)
 
-    for current_hour in range(total_hours):
-        current_datetime = first_date + timedelta(hours=current_hour)
+  for current_hour in range(total_hours):
+    current_datetime = first_date + timedelta(hours=current_hour)
 
-        folder_name = f'{str(current_datetime.month).zfill(2)}{str(current_datetime.day).zfill(2)}'
-        filename = f'd_{year[2:]}_{current_datetime.month}_{current_datetime.day}_{current_datetime.hour}.mat'
-        file_path = os.path.join(CommonPath.DATA_FOLDER.value, folder_name, filename)
+    folder_name = f'{str(current_datetime.month).zfill(2)}{str(current_datetime.day).zfill(2)}'
+    filename = f'd_{year[2:]}_{current_datetime.month}_{current_datetime.day}_{current_datetime.hour}.mat'
+    file_path = os.path.join(CommonPath.DATA_FOLDER.value, folder_name, filename)
 
-        
-        if os.path.isfile(file_path):
-            
-            data_mat = loadmat(file_path)
-            new_data = data_mat['Data'][:, sensor_number]
+    if os.path.isfile(file_path):
+      data_mat = loadmat(file_path)
+      new_data = data_mat['Data'][:, sensor_number]
 
-            data = stack_arrays(data, new_data)
-        
-            
+      data = stack_arrays(data, new_data)
 
-    return data
+  return data
--- src/damage_detector/utils.py
+++ src/damage_detector/utils.py
@@ -11,89 +11,88 @@ from src.damage_detector.Z24Dataset import load
 
 
 def __get_device() -> str:
-    """
+  """
     Gets the device string for TensorFlow operations, either GPU or CPU.
     """
-    if is_available():
-        current_device = 'cuda'
-    else:
-        current_device = 'cpu'
+  if is_available():
+    current_device = 'cuda'
+  else:
+    current_device = 'cpu'
 
-    print(current_device)
-    return current_device
+  print(current_device)
+  return current_device
 
 
 def split_in_sequences(data, sequence_length):
-    sample_length, n_samples = data.shape
-    sequence_samples_to_consider = (sample_length // sequence_length) * sequence_length
-    return data.T[:, :sequence_samples_to_consider].reshape((-1, sequence_length)).T
+  sample_length, n_samples = data.shape
+  sequence_samples_to_consider = (sample_length // sequence_length) * sequence_length
+  return data.T[:, :sequence_samples_to_consider].reshape((-1, sequence_length)).T
 
 
 def reshape_sequences(data: np.ndarray, new_length: int) -> np.ndarray:
-    new_data = data.flatten()
-    N_samples = new_data.shape[0]
-    samples_to_consider = N_samples - (N_samples % new_length)
+  new_data = data.flatten()
+  N_samples = new_data.shape[0]
+  samples_to_consider = N_samples - (N_samples % new_length)
 
-    return new_data[:samples_to_consider].reshape((-1, new_length))
+  return new_data[:samples_to_consider].reshape((-1, new_length))
 
 
 def build_model_folder_path(model_identifier: str, config_identifier: str, folder_name: str):
-    """
+  """
     Builds a directory path for a model and its configuration based on the provided identifiers
     @param model_identifier A string identifier for the current model.
     @param config_identifier A string identifier of the current model configuration.
     @param folder_name The folder name to save the runs.
     """
-    model_folder = os.path.join(folder_name, f'{model_identifier}_cnf_{config_identifier}')
-    return os.path.join(CommonPath.MODEL_PARAMETERS_FOLDER.value, model_folder)
-
+  model_folder = os.path.join(folder_name, f'{model_identifier}_cnf_{config_identifier}')
+  return os.path.join(CommonPath.MODEL_PARAMETERS_FOLDER.value, model_folder)
 
 
 def load_data(config_params: ConfigParams, is_train=True) -> tuple:
-    sequences_length = config_params.get_params('global_variables').get('sequences_length')
-    n_features = 1
+  sequences_length = config_params.get_params('global_variables').get('sequences_length')
+  n_features = 1
 
-    if is_train:
-        dataset_type = DatasetType.TRAIN_DATA
-        validation_dataset_type = DatasetType.VALIDATION_TRAIN_DATA
-        type_data_str = 'Train'
-    else:
-        dataset_type = DatasetType.TEST_DATA
-        validation_dataset_type = DatasetType.VALIDATION_TEST_DATA
-        type_data_str = 'Test'
+  if is_train:
+    dataset_type = DatasetType.TRAIN_DATA
+    validation_dataset_type = DatasetType.VALIDATION_TRAIN_DATA
+    type_data_str = 'Train'
+  else:
+    dataset_type = DatasetType.TEST_DATA
+    validation_dataset_type = DatasetType.VALIDATION_TEST_DATA
+    type_data_str = 'Test'
 
-    # Load data
-    data = load(config_params, dataset_type).T
-    validation_data = load(config_params, validation_dataset_type).T
-    print(f"{type_data_str} data shape (after the load): {data.shape}")
-    print(f"Vali data shape (after the load): {validation_data.shape}")
+  # Load data
+  data = load(config_params, dataset_type).T
+  validation_data = load(config_params, validation_dataset_type).T
+  print(f"{type_data_str} data shape (after the load): {data.shape}")
+  print(f"Vali data shape (after the load): {validation_data.shape}")
 
-    # Process the data
-    data = StandardScaler().fit_transform(data)
-    validation_data = StandardScaler().fit_transform(validation_data)
+  # Process the data
+  data = StandardScaler().fit_transform(data)
+  validation_data = StandardScaler().fit_transform(validation_data)
 
-    data = split_in_sequences(data, 1000)
-    validation_data = split_in_sequences(validation_data, 1000)
+  data = split_in_sequences(data, 1000)
+  validation_data = split_in_sequences(validation_data, 1000)
 
-    print(f"{type_data_str} data shape (after the sequence splitting): {data.shape}")
-    print(f"Vali data shape (after the sequence splitting): {validation_data.shape}")
+  print(f"{type_data_str} data shape (after the sequence splitting): {data.shape}")
+  print(f"Vali data shape (after the sequence splitting): {validation_data.shape}")
 
-    data = MaxAbsScaler().fit_transform(data).T
-    validation_data = MaxAbsScaler().fit_transform(validation_data).T
+  data = MaxAbsScaler().fit_transform(data).T
+  validation_data = MaxAbsScaler().fit_transform(validation_data).T
 
-    data = MinMaxScaler().fit_transform(data.T).T
-    validation_data = MinMaxScaler().fit_transform(validation_data.T).T
+  data = MinMaxScaler().fit_transform(data.T).T
+  validation_data = MinMaxScaler().fit_transform(validation_data.T).T
 
-    print(f"{type_data_str} data shape (after the scaling): {data.shape}")
-    print(f"Vali data shape (after the scaling): {validation_data.shape}")
+  print(f"{type_data_str} data shape (after the scaling): {data.shape}")
+  print(f"Vali data shape (after the scaling): {validation_data.shape}")
 
-    data = reshape_sequences(data, sequences_length)
-    validation_data = reshape_sequences(validation_data, sequences_length)
+  data = reshape_sequences(data, sequences_length)
+  validation_data = reshape_sequences(validation_data, sequences_length)
 
-    #data = data.reshape((-1, sequences_length, n_features))
-    #validation_data = validation_data.reshape((-1, sequences_length, n_features))
+  # data = data.reshape((-1, sequences_length, n_features))
+  # validation_data = validation_data.reshape((-1, sequences_length, n_features))
 
-    print(f"{type_data_str} data shape (after the adding the feature): {data.shape}")
-    print(f"Vali data shape (after the adding the feature): {validation_data.shape}")
+  print(f"{type_data_str} data shape (after the adding the feature): {data.shape}")
+  print(f"Vali data shape (after the adding the feature): {validation_data.shape}")
 
-    return data, validation_data
+  return data, validation_data
