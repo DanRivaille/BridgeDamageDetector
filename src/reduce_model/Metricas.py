be0c4d39f2ba76087c92d6e49741fbc6c158fcad
Dan Santos <dan.santos.hd@outlook.com>
2025-03-31 01:54:41 -0300

    Metricas.py file is reformated

--- src/reduce_model/Metricas.py
+++ src/reduce_model/Metricas.py
@@ -12,54 +12,55 @@ from src.models.Autoencoder import Autoencoder
 from src.models.CustomDataset import CustomDataset
 from src.damage_detector.utils import __get_device, build_model_folder_path, load_data
 
+
 def medir_tiempo_inferencia(modelo, validation_loader, device="cuda"):
-    modelo.to(device)
-    modelo.eval()
+  modelo.to(device)
+  modelo.eval()
 
-    criterion = nn.MSELoss()
+  criterion = nn.MSELoss()
 
-    start_time = time.time()
+  start_time = time.time()
 
-    for validation_batch in validation_loader:
-        validation_signals = validation_batch.to(device_to_use)
+  for validation_batch in validation_loader:
+    validation_signals = validation_batch.to(device)
 
-        val_output = model(validation_signals)
-        validation_loss = criterion(val_output, validation_signals.data)
-    
-   
-    elapsed_time = time.time() - start_time
+    val_output = modelo(validation_signals)
+    validation_loss = criterion(val_output, validation_signals.data)
 
-    return elapsed_time  
+  elapsed_time = time.time() - start_time
+
+  return elapsed_time
 
 
-args = ParserArguments()
-config_params = ConfigParams.load(os.path.join(CommonPath.CONFIG_FILES_FOLDER.value, args.config_filename))
-sequences_length = config_params.get_params('global_variables').get('sequences_length')
-train_data, validation_data = load_data(config_params, is_train=True)
+def main():
+  args = ParserArguments()
+  config_params = ConfigParams.load(os.path.join(CommonPath.CONFIG_FILES_FOLDER.value, args.config_filename))
+  sequences_length = config_params.get_params('global_variables').get('sequences_length')
+  train_data, validation_data = load_data(config_params, is_train=True)
 
-device_to_use = __get_device()
+  device_to_use = __get_device()
 
-batch_size = config_params.get_params('train_params')['batch_size']
+  batch_size = config_params.get_params('train_params')['batch_size']
 
-validation_set = CustomDataset(validation_data)
-validation_loader = DataLoader(validation_set, batch_size=batch_size, shuffle=False)
+  validation_set = CustomDataset(validation_data)
+  validation_loader = DataLoader(validation_set, batch_size=batch_size, shuffle=False)
 
-model_folder = build_model_folder_path(args.model_id, config_params.get_params('id'), args.folder_name)
+  model_folder = build_model_folder_path(args.model_id, config_params.get_params('id'), args.folder_name)
 
-"""
-Indicar el nombre del modelo a analizar las metricas.
-"""
-model_path = os.path.join(model_folder, 'model_reduced_ft.pth')
+  """
+    Indicar el nombre del modelo a analizar las metricas.
+    """
+  model_path = os.path.join(model_folder, 'model_reduced_ft.pth')
 
-model = Autoencoder(sequences_length)
-model.load_state_dict(torch.load(model_path))
-model.to(device_to_use)
-model.eval()
+  model = Autoencoder(sequences_length)
+  model.load_state_dict(torch.load(model_path))
+  model.to(device_to_use)
+  model.eval()
 
-memoria_antes = torch.cuda.memory_allocated(device_to_use) / (1024 * 1024)  
-print(f"Mmemoria al cargar el modelo: {memoria_antes}")
-tiempo_inferencia = medir_tiempo_inferencia(model, validation_loader)
-memoria_despues = torch.cuda.memory_allocated(device_to_use) / (1024 * 1024) 
+  memoria_antes = torch.cuda.memory_allocated(device_to_use) / (1024 * 1024)
+  print(f"Mmemoria al cargar el modelo: {memoria_antes}")
+  tiempo_inferencia = medir_tiempo_inferencia(model, validation_loader)
+  memoria_despues = torch.cuda.memory_allocated(device_to_use) / (1024 * 1024)
 
-print(f"Tiempo en inferencia  {tiempo_inferencia:.2f} Segundos")
-print(f"Mmemoria despues de inferencia: {memoria_despues}")
+  print(f"Tiempo en inferencia  {tiempo_inferencia:.2f} Segundos")
+  print(f"Mmemoria despues de inferencia: {memoria_despues}")
