e2d365773853a06f86492203b5681d1c623f512e
Dan Santos <dan.santos.hd@outlook.com>
2025-03-31 23:57:41 -0300

    BridgeObjectiveFunction.py file is cleaned

--- src/optimization/objective_function/BridgeObjectiveFunction.py
+++ src/optimization/objective_function/BridgeObjectiveFunction.py
@@ -1,126 +1,72 @@
-from src.optimization.objective_function.ObjectiveFunction import ObjectiveFunction
 import copy
-import numpy as np
 from torch.nn.utils import prune
 from torch import nn
-from torch.optim import Adam
+from torch import sum as torch_sum
+
+from src.optimization.objective_function.ObjectiveFunction import ObjectiveFunction
+
+
+def bitwise_operation(mask: list) -> int:
+  binary_mask = int(0)
+  for bit in mask:
+    binary_mask = (binary_mask << 1) | bit
+  return binary_mask
+
+
+def compute_key(mask, max_length) -> tuple:
+  num_sub_keys = max(1, len(mask) // max_length)
+
+  key = []
+  for i in range(num_sub_keys):
+    start_index = i * max_length
+    end_index = start_index + max_length
+    current_key = bitwise_operation(mask[start_index: end_index])
+    key.append(current_key)
+
+  return tuple(key)
 
 
 class BridgeObjectiveFunction(ObjectiveFunction):
-  def __init__(self, is_minimization: bool, model, train_loader, validation_loader, learning_rate, num_epochs,
-               device_to_use, proportion_rate):
+  def __init__(self, is_minimization: bool, model, train_loader, validation_loader, device_to_use, proportion_rate):
     super().__init__(is_minimization) 
     self.__model = model
     self.__train_loader = train_loader
     self.__validation_loader = validation_loader
-    self.__learning_rate = learning_rate
-    self.__num_epochs = num_epochs
     self.__device_to_use = device_to_use
     self.__proportion_rate = proportion_rate
     self.__historical_fitness = dict()
 
-  def train_mask_fine_tuning(self, model):
-    criterion = nn.MSELoss()
-    optimizer = Adam(model.parameters(), lr=self.__learning_rate)
+  def evaluate(self, mask) -> float:
+    current_key = compute_key(mask, 32)
 
-    train_error = []
-    validation_error = []
+    if current_key not in self.__historical_fitness:
+      self.__historical_fitness[current_key] = self.__compute_fitness(mask)
 
+    return self.__historical_fitness[current_key]
 
-    for epoch in range(self.__num_epochs):
-      valid_loss = 0
-           
-      for train_batch in self.__train_loader:
-          
-        signals = train_batch.to(self.__device_to_use)
+  def __compute_fitness(self, mask):
+    if (torch_sum(mask) / (mask.size(dim=0) * 1.0)) > self.__proportion_rate:
+      return 100.0
 
-        output = model(signals)
-        loss = criterion(output, signals.data)
+    pruned_model = copy.deepcopy(self.__model).to(self.__device_to_use)
+    self.__apply_pruning(pruned_model, mask)
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-      
+    return self.__evaluate_mask(pruned_model)
 
-      for validation_batch in self.__validation_loader:
-
-        validation_signals = validation_batch.to(self.__device_to_use)
-        # Revisar - calcular promedio
-        val_output = model(validation_signals)
-        validation_loss = criterion(val_output, validation_signals.data)
-        print(F"VALID LOSS {validation_loss}")
-        valid_loss += validation_loss.item()
-        
-      print()
-
-      train_error.append(loss.item())
-      validation_error.append(valid_loss/len(self.__validation_loader))
-      print(f"validation_loss = {valid_loss/len(self.__validation_loader)}")
-
-
-      print(f'epoch [{epoch + 1}/{self.__num_epochs}], loss:{loss.item(): .4f}')
-        
-    return validation_error[-1]
-  
-  def train_mask_evaluate_model(self, model):
+  def __evaluate_mask(self, model):
     criterion = nn.MSELoss()
     validation_error = []
 
     for validation_batch in self.__validation_loader:  
       validation_signals = validation_batch.to(self.__device_to_use)
 
-      # Revisar - calcular promedio
       val_output = model(validation_signals)
       validation_loss = criterion(val_output, validation_signals.data)
       validation_error.append(validation_loss.item())
 
-    promedio_loss = sum(validation_error)/len(validation_error)
-    #print(f"validation_loss = {promedio_loss}")
+    return sum(validation_error) / len(validation_error)
 
-
-    return promedio_loss
-
-  def bitwise_operation(self, mask: list) -> int:
-    binary_mask = int(0)
-    for bit in mask:
-      binary_mask = (binary_mask << 1) | bit
-    return binary_mask
-
-  def compute_key(self, mask, max_length) -> tuple:
-    lenght_mask = len(mask)
-    num_sub_keys = max(1, lenght_mask // max_length)
-
-    key = []
-    for i in range(num_sub_keys):
-      start_index = i * max_length
-      end_index = start_index + max_length
-      current_key = self.bitwise_operation(mask[start_index: end_index])
-      key.append(current_key)
-
-    return tuple(key)
-
-  def evaluate(self, mask) -> float:
-    current_key = self.compute_key(mask, 32)
-
-    if current_key not in self.__historical_fitness:
-      array = mask.numpy()
-      weigh_count = np.sum(array)
-      largo = np.size(array)
-      if (weigh_count/largo) > self.__proportion_rate:
-        print("invalido")
-        return 100.0
-
-      modelo_copia = copy.deepcopy(self.__model)
-      modelo_copia = modelo_copia.to(self.__device_to_use)
-
-      self.apply_pruning(modelo_copia, mask)
-
-      validation_error = self.train_mask_evaluate_model(modelo_copia)
-      self.__historical_fitness[current_key] = validation_error
-
-    return self.__historical_fitness[current_key]
-
-  def apply_pruning(self, modelo, mask):
+  def __apply_pruning(self, modelo, mask):
     mask = mask.to(self.__device_to_use)
 
     if modelo.layer_to_mask == "first":
@@ -136,6 +82,4 @@ class BridgeObjectiveFunction(ObjectiveFunction):
       prune.custom_from_mask(modelo.decoder[0], name='weight', mask=mask)
 
   def get_history(self) -> dict:
-    sort_history = dict(sorted(self.__historical_fitness.items(), key=lambda item: item[1], reverse=True))
-
-    return sort_history
+    return dict(sorted(self.__historical_fitness.items(), key=lambda item: item[1], reverse=True))
