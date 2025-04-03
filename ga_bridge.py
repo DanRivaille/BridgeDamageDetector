5bd6010ad4603c89d4dc611bc4b50121ae3df7d7
Dan Santos <dan.santos.hd@outlook.com>
2025-04-02 21:19:32 -0300

    Clasical pruning techniques are implemented in ga_bridge

--- ga_bridge.py
+++ ga_bridge.py
@@ -1,8 +1,10 @@
 import os
 import time
 import logging
+import copy
 
 from torch.utils.data import DataLoader
+from torch.nn.utils import prune
 
 import torch
 from torch import load
@@ -21,58 +23,79 @@ from src.optimization.objective_function.BridgeObjectiveFunction import BridgeOb
 from src.optimization.genetic_algorithm.movements_supplier.BridgeMovementsSupplier import BridgeMovementSupplier
 
 if __name__ == '__main__':
-    args = ParserArguments()
+  args = ParserArguments()
 
-    config_params = ConfigParams.load(os.path.join(CommonPath.CONFIG_FILES_FOLDER.value, args.config_filename))
-    sequences_length = config_params.get_params('global_variables').get('sequences_length')
+  config_params = ConfigParams.load(os.path.join(CommonPath.CONFIG_FILES_FOLDER.value, args.config_filename))
+  sequences_length = config_params.get_params('global_variables').get('sequences_length')
 
-    train_data, validation_data = load_data(config_params, is_train=True)
+  train_data, validation_data = load_data(config_params, is_train=True)
 
-    device_to_use = __get_device()
-    print(device_to_use)
+  device_to_use = __get_device()
+  print(device_to_use)
 
-    batch_size = config_params.get_params('train_params')['batch_size']
-    to_mask = config_params.get_params('ga_params')['to_mask']
+  batch_size = config_params.get_params('train_params')['batch_size']
+  to_mask = config_params.get_params('ga_params')['to_mask']
 
-    n_genes = config_params.get_params('mask_n_genes')[to_mask]
-    population_size = config_params.get_params('ga_params')['population_size']
-    n_generations = config_params.get_params('ga_params')['n_generations']
-    p_mutate = config_params.get_params('ga_params')['p_mutate']
-    p_cross = config_params.get_params('ga_params')['p_cross']
-    proportion_rate = config_params.get_params('ga_params')['proportion_rate']
+  n_genes = config_params.get_params('mask_n_genes')[to_mask]
+  population_size = config_params.get_params('ga_params')['population_size']
+  n_generations = config_params.get_params('ga_params')['n_generations']
+  p_mutate = config_params.get_params('ga_params')['p_mutate']
+  p_cross = config_params.get_params('ga_params')['p_cross']
+  proportion_rate = config_params.get_params('ga_params')['proportion_rate']
 
-    validation_set = CustomDataset(validation_data)
-    validation_loader = DataLoader(validation_set, batch_size=batch_size, shuffle=False)
+  validation_set = CustomDataset(validation_data)
+  validation_loader = DataLoader(validation_set, batch_size=batch_size, shuffle=False)
 
-    #model_folder = build_model_folder_path(args.model_id, config_params.get_params('id'), args.folder_name)
-    model_folder = build_model_folder_path(args.model_id, 'full_config_ae', args.folder_name)
-    model_path = os.path.join(model_folder, 'model_trained.pth')
+  model_folder = build_model_folder_path(args.model_id, config_params.get_params('id'), args.folder_name)
+  model_path = os.path.join(model_folder, 'model_trained.pth')
 
-    model = Autoencoder(sequences_length, to_mask)
-    model.load_state_dict(load(model_path))
-    model.eval()
-    model.to(device_to_use)
+  model = Autoencoder(sequences_length, to_mask)
+  model.load_state_dict(load(model_path))
+  model.eval()
+  model.to(device_to_use)
 
-    # Entrenar modelo.
-    ga_params: GAParameters = GAParameters(population_size, n_genes, n_generations, p_mutate, p_cross, proportion_rate)
-    bridge_obj_function: ObjectiveFunction = BridgeObjectiveFunction(True, model, validation_loader,
-                                                                     device_to_use, proportion_rate)
-    bridge_movement_supplier: GAMovementsSupplier = BridgeMovementSupplier(ga_params)
+  # Entrenar modelo.
+  ga_params: GAParameters = GAParameters(population_size, n_genes, n_generations, p_mutate, p_cross, proportion_rate)
+  bridge_obj_function: ObjectiveFunction = BridgeObjectiveFunction(True, model, validation_loader,
+                                                                   device_to_use, proportion_rate)
+  bridge_movement_supplier: GAMovementsSupplier = BridgeMovementSupplier(ga_params)
 
-    genetic_algorithm: OptimizationAlgorithm = GeneticAlgorithm(ga_params, bridge_movement_supplier, bridge_obj_function)
+  genetic_algorithm: OptimizationAlgorithm = GeneticAlgorithm(ga_params, bridge_movement_supplier, bridge_obj_function)
 
-    # Base fitness
-    base_model_fitness = evaluate_model(model, validation_loader, device_to_use)
-    logging.warning(f'Base model fitness: {base_model_fitness}')
+  # Base fitness
+  base_model_fitness = evaluate_model(model, validation_loader, device_to_use)
+  logging.warning(f'Base model fitness: {base_model_fitness}')
 
-    start_time = time.time()
-    best_solution, best_fitness = genetic_algorithm.run()
-    print(f"Tiempo de ejecucion = {time.time() - start_time}")
-    print(f"Best Fitness = {best_fitness}")
-    print(best_solution)
+  pruned_model_l1 = copy.deepcopy(model).to(device_to_use)
+  pruned_model_l2 = copy.deepcopy(model).to(device_to_use)
 
-    if args.save:
-        # Save the results
-        pass
+  # Prune estructurado eliminando filas (neuronas de salida)
+  prune.ln_structured(
+    module=pruned_model_l1.decoder[0],  # Primera capa
+    name='weight',
+    amount=1 - proportion_rate,  # Proporción de filas (neuronas) a eliminar
+    n=1,  # Usamos norma L1 para determinar importancia
+    dim=0)  # Filas: Desactiva neuronas completas
 
-        
+  # Prune estructurado eliminando filas (neuronas de salida)
+  prune.ln_structured(
+    module=pruned_model_l2.decoder[0],
+    name='weight',
+    amount=1 - proportion_rate,
+    n=2,
+    dim=0)
+
+  mean_loss_l1 = evaluate_model(pruned_model_l1, validation_loader, device_to_use)
+  mean_loss_l2 = evaluate_model(pruned_model_l2, validation_loader, device_to_use)
+  print(f"Best validation loss (L1) {mean_loss_l1}")
+  print(f"Best validation loss (L2) {mean_loss_l2}")
+
+  start_time = time.time()
+  best_solution, best_fitness = genetic_algorithm.run()
+  print(f"Tiempo de ejecucion = {time.time() - start_time}")
+  print(f"Best Fitness = {best_fitness}")
+  print(best_solution)
+
+  if args.save:
+    # Save the results
+    pass
